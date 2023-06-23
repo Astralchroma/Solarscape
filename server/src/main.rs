aed11bf72102854d5b7f6756953a8cf53bde45b9
Astralchroma <astralchroma@proton.me>
2023-06-23 19:48:40 +0100

    Properly handle errors from `await_connections()`

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,11 +8,11 @@ use tokio_util::sync::CancellationToken;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+	let token = CancellationToken::new();
+
 	// If a Tokio task panics, Tokio will catch the panic with the intent that the caller will handle it.
 	// However, generally you shouldn't try to recover a panic, instead your goal should be to safely exit.
-	let hook_token = CancellationToken::new();
-	let token = hook_token.clone();
-
+	let hook_token = token.clone();
 	let default_panic = panic::take_hook();
 	panic::set_hook(Box::new(move |info| {
 		default_panic(info);
@@ -31,7 +31,14 @@ async fn main() -> Result<()> {
 		env::set_current_dir(data)?;
 	}
 
-	tokio::spawn(Server::new().await_connections());
+	let server_token = token.clone();
+	tokio::spawn(async move {
+		let result = Server::new().await_connections().await;
+		server_token.cancel();
+		if let Err(error) = result {
+			eprintln!("{error:?}")
+		}
+	});
 
 	token.cancelled_owned().await;
 	Ok(())
