f8fd456dbb2a88e5a6ca6ac8400be1c65e0bca8a
Astralchroma <astralchroma@proton.me>
2023-10-06 10:50:34 +0100

    Remove unused function

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -18,11 +18,3 @@ pub fn shared_main() -> Result<Runtime> {
 
 	tokio::runtime::Builder::new_multi_thread().enable_all().build()
 }
-
-/// Shorthand to be used in place of `..Default::default()`. This is a copy of Rust's "default_free_fn" unstable feature
-/// because we don't want to use nightly, plus they seem to be removing it anyway.
-#[must_use]
-#[inline]
-pub fn default<T: Default>() -> T {
-	Default::default()
-}
