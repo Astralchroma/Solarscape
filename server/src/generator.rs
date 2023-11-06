3faa3c64f83da2f4d47b8507d0b95dcec3206afc
Astralchroma <astralchroma@proton.me>
2023-11-06 13:42:42 +0000

    server: bit shift not multiply

--- server/src/generator.rs
+++ server/src/generator.rs
@@ -13,15 +13,18 @@ pub struct SphereGenerator {
 impl Generator for SphereGenerator {
 	fn generate_chunk(&self, object: Entity, scale: u8, grid_position: Vector3<i32>) -> Chunk {
 		let mut chunk = Chunk::empty(object, grid_position);
-		let chunk_position = (grid_position * (16 * scale as i32)).cast();
+		let chunk_position = (grid_position * (16 << scale)).cast();
 
-		for x in (0..16 * scale).step_by(scale as usize) {
-			for y in (0..16 * scale).step_by(scale as usize) {
-				for z in (0..16 * scale).step_by(scale as usize) {
+		for x in (0..16 << scale).step_by(scale as usize + 1) {
+			for y in (0..16 << scale).step_by(scale as usize + 1) {
+				for z in (0..16 << scale).step_by(scale as usize + 1) {
 					let cell_position = Vector3::new(x as f32 + 0.5, y as f32 + 0.5, z as f32 + 0.5) + chunk_position;
 					let distance = cell_position.metric_distance(&Vector3::new(0.0, 0.0, 0.0));
 
-					chunk.set(&Vector3::new(x / scale, y / scale, z / scale), distance < self.radius);
+					chunk.set(
+						&Vector3::new(x >> scale, y >> scale, z >> scale),
+						distance < self.radius,
+					);
 				}
 			}
 		}
