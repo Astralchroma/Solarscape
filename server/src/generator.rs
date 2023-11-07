cfd05cfa212ab6962f7b5c176f4184b09bbb35a6
Astralchroma <astralchroma@proton.me>
2023-11-07 19:41:10 +0000

    remove properly unnecessary code?

--- server/src/generator.rs
+++ server/src/generator.rs
@@ -14,12 +14,11 @@ impl Generator for SphereGenerator {
 	fn generate_chunk(&self, object: Entity, scale: u8, grid_position: Vector3<i32>) -> Chunk {
 		let mut chunk = Chunk::empty(object, scale, grid_position);
 		let chunk_position = (grid_position * (16 << scale)).cast();
-		let center_offset = Vector3::new(scale as f32 / 2.0, scale as f32 / 2.0, scale as f32 / 2.0);
 
 		for x in (0..16 << scale).step_by(scale as usize + 1) {
 			for y in (0..16 << scale).step_by(scale as usize + 1) {
 				for z in (0..16 << scale).step_by(scale as usize + 1) {
-					let cell_position = Vector3::new(x as f32, y as f32, z as f32) + center_offset + chunk_position;
+					let cell_position = Vector3::new(x as f32, y as f32, z as f32) + chunk_position;
 					let distance = cell_position.metric_distance(&Vector3::new(0.0, 0.0, 0.0));
 
 					chunk.set(
