b7fea921761dd8dc609a6ac4dd60943ca8750b14
Astralchroma <astralchroma@proton.me>
2023-11-24 19:23:31 +0000

    Temporarily disable LOD generation

--- server/src/voxel_object.rs
+++ server/src/voxel_object.rs
@@ -5,7 +5,7 @@ use solarscape_shared::protocol::{encode, Message, SyncEntity};
 use solarscape_shared::{chunk::Chunk, components::Location};
 
 // TODO: anything higher than 8 causes overflow, look into this later
-pub const OCTREE_LEVELS: u8 = 8;
+pub const OCTREE_LEVELS: u8 = 1;
 
 // Temporary
 pub const CHUNK_RADIUS: i32 = 3;
