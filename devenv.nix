1c8aae128f5bbc0e2dafd856fe2386b3994e155a
Astralchroma <astralchroma@proton.me>
2024-06-04 22:34:24 +0100

    Add runtime dependencies to devenv

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,10 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
 	languages.rust = {
 		enable = true;
 		channel = "stable";
 	};
+
+	env.LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
+		vulkan-loader xorg.libX11 xorg.libXcursor xorg.libXi libxkbcommon xorg.libXrandr wayland pkg-config
+	]);
 }
