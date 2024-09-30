57bc39986db503f27f056eacd72e025135803839
Astralchroma <astralchroma@proton.me>
2024-09-30 23:40:39 +0100

    Super basic, technically working account creation

--- devenv.nix
+++ devenv.nix
@@ -4,9 +4,13 @@
 		channel = "stable";
 	};
 
-	env.LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
-		libxkbcommon
-		vulkan-loader
-		wayland
-	]);
+	services.postgres = {
+		enable = true;
+		package = pkgs.postgresql_16;
+		initialDatabases = [{ name = "solarscape"; }];
+		listen_addresses = "127.0.0.1";
+	};
+
+	packages = with pkgs; [ openssl pkg-config sqlx-cli ];
+	env.LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [ libxkbcommon vulkan-loader wayland ]);
 }
